7ad13f6c8b7eb6b0bffe940da7006ad532609e05
Mohammad Moiz Ali <moizali@kerloresearch.com>
2024-01-23 20:02:51 +0500

    add x-forwarded-for header

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ app.enable('trust proxy'); // This is necessary if you're behind a reverse proxy
 
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    console.log(req.clientIp)
+    console.log(req.headers, req.socket)
     // res.send('Yo!')
-    const clientIp = req.clientIp;
+    // const clientIp = req.clientIp;
+    const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
     res.json({ ip: clientIp });
 })
 app.listen(process.env.PORT || 3000)
