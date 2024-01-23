bd45ce50eaca08081b3ac0de9e1a4503c3c1db03
Mohammad Moiz Ali <moizali@kerloresearch.com>
2024-01-23 19:50:59 +0500

    add trust proxy

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+
+app.enable('trust proxy'); // This is necessary if you're behind a reverse proxy like Heroku
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
+    console.log(req.clientIp)
     // res.send('Yo!')
     const clientIp = req.clientIp;
     res.json({ ip: clientIp });
